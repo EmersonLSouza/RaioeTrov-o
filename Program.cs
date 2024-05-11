bfafa2b6bebe6367c20de95b397edd31da167af2
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-10 22:30:59 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿double tempo, distancia, velocidade = 340.29, horasExtras;
+
+Console.Clear();
+Console.WriteLine("---Raio e Trovão---\n");
+
+tempo = SolicitarValor("Informe o atraso entre raio e o trovão: ");
+
+distancia = Calculartempo(velocidade, tempo);
+
+Console.WriteLine($"a distancia entre você e o raio é: {distancia:N2}KM");
+
+double Calculartempo(double velocidade
+                      , double tempo)
+{
+    double distancia = (tempo * velocidade) / 1000;
+    return distancia;
+}
+
+double SolicitarValor(string mensagem)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.Write($"{mensagem} ");
+    Console.ResetColor();
+    return Convert.ToDouble(Console.ReadLine());
+}
